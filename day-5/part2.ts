9f73febf7c3bb915e7867d262d7e07ea5fe07c7f
Joe Roxbury <roxbury.joseph@gmail.com>
2022-12-03 12:36:48 -0700

    generate day 4/5 with setup script

---
+++ day-5/part2.ts
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+// let data = fs.readFileSync(path.resolve(__dirname, "data.txt")).toString();
+// let data = fs.readFileSync(path.resolve(__dirname, "data-example.txt")).toString();
+
+export function part2() {
+  console.log("day - part 2");
+}
--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-import { template } from "./template";
 import { day3 } from "./day-3";
-// template();
 // day3();
+
+import { day4 } from "./day-4";
+day4();
+
+import { day5 } from "./day-5";
+// day5();
