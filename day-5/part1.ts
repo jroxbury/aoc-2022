e1e6ea4db02216e64c6957148fe3854d9dec54f4
Joe Roxbury <roxbury.joseph@gmail.com>
2022-12-05 00:25:57 -0700

    day 5

---
+++ day-5/part1.ts
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+let data = fs
+  .readFileSync(path.resolve(__dirname, "data.txt"))
+  .toString()
+  .split("\n");
+
+export function part1(): void {
+  const separator = data.findIndex((item) => item === "");
+  const columns = data
+    .slice(separator - 1, separator)[0]
+    .trim()
+    .split("  ")
+    .map((item) => item.trim());
+  const numColumns = Number(columns[columns.length - 1]);
+  const stackData = data.slice(0, separator - 1);
+  const instructions = data
+    .slice(separator + 1)
+    .map((instruction) => parseInstructions(instruction));
+  const stacks = [] as unknown as [string[]];
+
+  for (let index = 0; index < numColumns; index++) {
+    stacks.push([]);
+  }
+
+  for (let i = 0; i < stackData.length; i++) {
+    getStackValue(stackData[i]);
+  }
+
+  for (const instruct of instructions) {
+    let [count, from, to] = instruct;
+    from -= 1;
+    to -= 1;
+    for (let index = 0; index < count; index++) {
+      stacks[to].push(stacks[from].pop());
+    }
+  }
+
+  let str = "";
+  for (const row of stacks) {
+    str += row.pop();
+  }
+  console.log(str);
+
+  function getStackValue(row: string) {
+    let counter = 1;
+    for (let index = 0; index < numColumns; index++) {
+      if (typeof row[counter] === "string" && row[counter] != " ") {
+        stacks[index].unshift(row[counter]);
+      }
+      counter += 4;
+    }
+  }
+
+  function parseInstructions(instructions: string) {
+    const regex = /[\s\d\s]+/g;
+    return instructions.match(regex).map((num) => Number(num));
+  }
+}
--- index.ts
+++ index.ts
@@ -8,4 +8,7 @@ import { day3 } from "./day-3";
 // day3();
 
 import { day4 } from "./day-4";
-day4();
+// day4();
+
+import { day5 } from "./day-5";
+day5();
